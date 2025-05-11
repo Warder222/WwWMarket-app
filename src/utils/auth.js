894bd2f12025702343ba731b223f8534919b095b
Warder222 <chugaynov.2005@mail.ru>
2025-05-11 22:40:06 +0500

    Updates

--- src/utils/auth.js
+++
@@ -1,8 +0,0 @@
-// src/utils/auth.js
-export const getUserData = () => {
-  if (window.Telegram && window.Telegram.WebApp) {
-    window.Telegram.WebApp.ready();
-    return window.Telegram.WebApp.initDataUnsafe.user;
-  }
-  return null;
-};
